106a3ace346237dada282bb4f69569fa7dce98d6
Leonardo Romeiro <leozinwork@gmail.com>
2024-02-20 18:00:10 -0300

    Desenvolvendo modal

---
+++ Components/Dialog.razor.js
@@ -0,0 +1,9 @@
+﻿window.showModal() {
+    let el document.getElementById('result-dialog');
+    el.showModal;
+}
+
+window.hideModal() {
+    let el document.getElementById('result-dialog');
+    el.close;
+}
