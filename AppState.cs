11ea2a36a58e875d74cb6a8c3a4ec75c8f398de2
Leonardo Romeiro <leozinwork@gmail.com>
2024-02-22 17:53:03 -0300

    Funcionando

--- AppState.cs
+++ AppState.cs
@@ -4,7 +4,7 @@ namespace Imc
 {
     public static class AppState
     {
-        public static List<ImcModel> History { get; set; } = new ();
+        public static List<ImcModel> History { get; set; } = new();
         public static event EventHandler<EventArgs> OnChanged;
 
         public static void NotifyChanges(object? sender, EventArgs args)
