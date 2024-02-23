01d40de9fac7cd571f97150a391609001ec2aa7a
Leonardo Romeiro <leozinwork@gmail.com>
2024-02-23 18:01:36 -0300

    Finalizando expediente corrido

--- AppState.cs
+++ AppState.cs
@@ -1,15 +1,46 @@
-﻿using Imc.Models;
+﻿using Blazored.LocalStorage;
+using Imc.Models;
+using Imc.ViewModels;
+using System.Reflection;
 
 namespace Imc
 {
     public static class AppState
     {
         public static List<ImcModel> History { get; set; } = new();
+        public static List<IMCViewModel> ViewHistory { get; set; } = new();
         public static event EventHandler<EventArgs> OnChanged;
 
-        public static void NotifyChanges(object? sender, EventArgs args)
+        public static async Task NotifyChanges(object? sender, EventArgs args)
         {
             OnChanged.Invoke(sender, args);
+            UpdateView();
+        }
+
+        public static async Task UpdateHistoryAsync(ILocalStorageService localStorage)
+        {
+            var data = await localStorage.GetItemAsync<List<ImcModel>?>("data");
+            if (data is not null)
+                History = data;
+            UpdateView();
+        }
+
+        public static void UpdateView()
+        {
+            ViewHistory.Clear();
+            foreach (var imc in History)
+            {
+                var imcViewModel = new IMCViewModel
+                {
+                    Registered = imc.CreatedAt,
+                    Value = Math.Round((decimal)imc.Imc, 1),
+                    Status = imc.Status,
+                    Weight = imc.Weight,
+                    Height = imc.Height
+                };
+
+                ViewHistory.Add(imcViewModel);
+            }
         }
     }
 }
---
+++ _AppState.cs
@@ -0,0 +1,54 @@
+﻿using Blazored.LocalStorage;
+using Imc.Models;
+using Imc.ViewModels;
+using System.Reflection;
+
+namespace Imc
+{
+    public static class _AppState
+    {
+		private static ILocalStorageService _localStorage;
+		public static async Task Initialize(ILocalStorageService localStorage)
+		{
+			_localStorage = localStorage;
+
+			var data = await _localStorage.GetItemAsync<List<ImcModel>>("data");
+			if (data != null)
+			{
+				_history = data;
+				UpdateHistoryViewModel();
+			}
+		}
+		public static List<ImcModel> _history { get; set; } = new();
+		public static List<IMCViewModel> History { get; set; } = new();
+		public static event EventHandler<EventArgs> OnChanged;
+
+        public static void NotifyChanges(object? sender, EventArgs args)
+        {
+            OnChanged.Invoke(sender, args);
+        }
+
+		private static void UpdateHistoryViewModel()
+		{
+			History.Clear();
+			foreach (var imc in _history)
+			{
+				var imcViewModel = new IMCViewModel
+				{
+					Registered = imc.CreatedAt,
+					Value = Math.Round((decimal)imc.Imc, 1),
+					Status = imc.Status,
+					Weight = imc.Weight,
+					Height = imc.Height
+				};
+
+				History.Add(imcViewModel);
+			}
+		}
+        public static async Task AddImcAsync(ImcModel model)
+        {
+            _history.Add(model);
+            await _localStorage.SetItemAsync("data", _history);
+        }
+    }
+}
